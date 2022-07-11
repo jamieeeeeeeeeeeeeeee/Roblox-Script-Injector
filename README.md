b8537ce46419be1bd43b9eb4ec0b1a4ae5b0dccd
jamie <jammieeeeeewhite@gmail.com>
2022-07-11 23:21:24 +0100

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # robloxx
-Roblox injector
+Roblox script injector
+
+11/07/2022 made in 2019, I didn't use GitHub back then
---
+++ robloxx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeAreDevs_API;
+
+
+namespace robloxx
+{
+    public partial class robloxx : Form
+    {
+        WeAreDevs_API.ExploitAPI api = new WeAreDevs_API.ExploitAPI();
+        public robloxx()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            api.LaunchExploit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            api.SendLuaScript(fastColoredTextBox1.Text);
+        }
+
+        private void fastColoredTextBox1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
